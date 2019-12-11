5aa0744f6f179fa238c3ebd778d44bc56305d307
Sridhar Katta <kattasridhar02@gmail.com>
2019-12-11 16:34:03 +0530

    implementing onkeyDown on the form submit method.

--- src/todo.js
+++ src/todo.js
@@ -101,12 +101,14 @@ class _Todo extends Component {
   };
 
   handleSubmit = event => {
-    event.preventDefault();
-    this.setState({
-      item: '',
-      todoList: [...this.state.todoList, this.state.item],
-      added: false
-    });
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      this.setState({
+        item: '',
+        todoList: [...this.state.todoList, this.state.item],
+        added: false
+      });
+    }
   };
 
   componentDidUpdate() {
@@ -124,15 +126,12 @@ class _Todo extends Component {
     const { classes } = this.props;
     const { todoList, added } = this.state;
     const addItem = added ? (
-      <form onSubmit={this.handleSubmit} className={classes.form}>
+      <form onKeyDown={this.handleSubmit} className={classes.form}>
         <input
           className={classes.input}
           value={this.state.item}
           onChange={this.handleChange}
         />
-        <Button type="submit" hidden>
-          submit
-        </Button>
       </form>
     ) : (
       <div></div>
